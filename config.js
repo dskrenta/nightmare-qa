29600478ee75831fd890614555e2024acfb9a646
David Skrenta <dskrenta@gmail.com>
2016-07-11 05:09:50 -0700

    image onload and onerror with cookies

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const config = {
-  'host': 'http://stars.topix.com',
-  'layoutMode': 'prod-active',
-  'itemid': 16088,
-  'screenshots': false,
-  'slides': 10,
-  'windowWidth': 1366,
-  'windowHeight': 768,
-  'waitBetweenSlides': 2500
+  host: 'http://stars.topix.com',
+  layoutMode: 'prod-active',
+  itemid: 16088,
+  screenshots: false,
+  slides: 10,
+  windowWidth: 1366,
+  windowHeight: 768,
+  waitBetweenSlides: 2500
 };
 
 module.exports = config;
--- stars.js
+++ stars.js
@@ -9,6 +9,8 @@ const nightmare = Nightmare({
 const path = require('path');
 const config = require(path.join(__dirname, 'config.js'));
 
+let numImages = 0;
+
 vo(run)(function(err, result) {
   if (err) throw err;
 });
@@ -19,10 +21,28 @@ function *run () {
     .goto(`${config.host}/slideshow/${config.itemid}/?layoutmode=${config.layoutMode}`);
 
   for (let i = 0; i < config.slides; i++) {
-    var result = yield *loopThroughSlides(i);
+    if (i === 0) {
+      numImages = yield *loopThroughSlides(i);
+    } else {
+      yield *loopThroughSlides(i);
+    }
+  }
+
+  for (let i = 1; i < numImages; i++) {
+    let cookie = yield getCookie(`${i}image`);
+    console.log(cookie);
   }
 
   yield nightmare.end();
+}
+
+function *getCookie (name) {
+  let cookie = yield nightmare
+    .cookies.get({ url: null, name: name })
+    .then((cookie) => {
+      return cookie;
+    });
+  return cookie;
 }
 
 function *loopThroughSlides (i) {
@@ -32,8 +52,6 @@ function *loopThroughSlides (i) {
       .click('.x-text')
       .wait(config.waitBetweenSlides)
       .evaluate(() => {
-        // instead of grabbing img src's register onload events for every image which store data in a cookie
-
         let topCaptionsList = document.querySelectorAll('.js-slide-caption');
         let imagesList = document.querySelectorAll('.js-slide-img');
         let slideCaptionsList = document.querySelectorAll('.x-slide-subcaption');
@@ -43,15 +61,22 @@ function *loopThroughSlides (i) {
         let slideCaptions = Array.from(slideCaptionsList);
 
         topCaptions.forEach((item, index) => {
-          topCaptions[index] = item.textContent;
+          topCaptions[index] = item.textContent.trim();
         });
 
         images.forEach((item, index) => {
-          images[index] = item.src;
+          if (item.src != 'http://static.topixcdn.com/px/images/blank.png') {
+            item.onerror = () => {
+              document.cookie = `${index}image=error`;
+            }
+            item.onload = () => {
+              document.cookie = `${index}image=load`;
+            }
+          }
         });
 
         slideCaptions.forEach((item, index) => {
-          slideCaptions[index] = item.textContent;
+          slideCaptions[index] = item.textContent.trim();
         });
 
         return {
@@ -61,22 +86,38 @@ function *loopThroughSlides (i) {
         };
       });
 
-    result.topCaptions.forEach((item) => {
-      console.log(item);
+    result.topCaptions.forEach((item, index) => {
+      console.log(`${item} \n`);
     });
 
-    result.images.forEach((item) => {
-      console.log(item);
+    result.images.forEach((item, index) => {
+      console.log(`${item} \n`);
     });
 
-    result.slideCaptions.forEach((item) => {
-      console.log(item);
+    result.slideCaptions.forEach((item, index) => {
+      console.log(`${item} \n`);
     });
+
+    return result.images.length;
 
   } else {
     yield nightmare
       .click('.js-next')
-      .wait(config.waitBetweenSlides);
+      .wait(config.waitBetweenSlides)
+      .evaluate(() => {
+        let imagesList = document.querySelectorAll('.js-slide-img');
+        let images = Array.from(imagesList);
+        images.forEach((item, index) => {
+          if (item.src != 'http://static.topixcdn.com/px/images/blank.png') {
+            item.onerror = () => {
+              document.cookie = `${index}image=error`;
+            }
+            item.onload = () => {
+              document.cookie = `${index}image=load`;
+            }
+          }
+        });
+      });
   }
 
   if (config.screenshots) {
